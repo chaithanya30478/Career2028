e9a92e716bdec7395ca8cece6b994f14a30bc746
chaithanya30478 <chaithustudy24@gmail.com>
2026-07-17 22:01:27 +0530

    Create strings.py

---
+++ Week1-Day2/strings.py
@@ -0,0 +1,12 @@
+x="Hello World"
+y="Welcome to Python Programming"
+print(x)
+print(len(x))
+uppercase_x=x.upper()
+print (uppercase_x)
+uppercase_y=y.upper()
+print (uppercase_y)
+lowercase_x=x.lower()
+print(lowercase_x)
+lowercase_y=y.lower()
+print(lowercase_y)
