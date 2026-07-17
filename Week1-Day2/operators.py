91a7795822c6b36921518193971bab1f7c827f63
chaithanya30478 <chaithustudy24@gmail.com>
2026-07-17 22:00:10 +0530

    Create operators.py

---
+++ Week1-Day2/operators.py
@@ -0,0 +1,18 @@
+x=input("Enter first number: ")
+y=input("Enter second number: ")
+x=int(x)
+y=int(y)
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+e=x//y
+f=x%y
+g=x**y
+print("Addition: ",a)
+print("Subtraction: ",b)
+print("Multiplication: ",c)
+print("Division: ",d)
+print("Floor Division: ",e)
+print("Modulus: ",f)
+print("Exponentiation: ",g)
